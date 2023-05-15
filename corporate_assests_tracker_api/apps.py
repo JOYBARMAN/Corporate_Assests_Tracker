0579f732feeebdfd4b4d97a587a019aac9588358
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2023-05-15 18:24:29 +0600

    Start project Corporate_Assests_Tracker | Root folder = config | Apps = corporate_assests_tracker_api

---
+++ corporate_assests_tracker_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CorporateAssestsTrackerApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'corporate_assests_tracker_api'
